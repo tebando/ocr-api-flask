518cda26b12e9e5d98825a6fadecacdae0d9a6bf
tebando <45377127+tebando@users.noreply.github.com>
2025-05-28 14:49:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ from pdf2image import convert_from_bytes
 
 app = Flask(__name__)
 
-@app.route("/", methods=["POST"])
+@app.route("/", methods=["GET", "POST"])
 def ocr_pdf():
+    if request.method == "GET":
+        return "OCR API is running."
+
     pdf_data = request.data
     images = convert_from_bytes(pdf_data, first_page=1, last_page=1)
     text = pytesseract.image_to_string(images[0])
